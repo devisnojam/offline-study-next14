0763277e74d14017b99e9dbd3a2eec55bedf1a1b
2dubbing <2dubbing@gmail.com>
2024-10-01 14:33:51 +0900

    refactor: 소스코드를 src 디렉토리 하위로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <RootProviders>{children}</RootProviders>
       </body>
     </html>
